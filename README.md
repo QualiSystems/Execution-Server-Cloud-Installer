b2462c4089167908e848d0c889282baf462a5724
alexquali <alexey.b@quali.com>
2023-01-16 15:51:52 +0200

    Added Ubuntu installation support

--- README.md
+++ README.md
@@ -8,9 +8,13 @@ The content of this repository is not intended to be used "as is". Please refer 
 - ExecutionServer.tar
 - Python-2.7.18.tgz
 - Python-3.9.9.tgz
-- rpm_pkgs
+- os_pkgs
     - binutils.rpm
     - mono-complete-5.16.0.220-0.xamarin.4.epel7.x86_64.rpm
+    - ...
+
+    - binfmt-support_2.2.1-2_amd64.deb
+    - mono-complete_6.12.0.182-0xamarin1+ubuntu2004b1_amd64.deb
     - ...
 - python_pkgs
     - pip-19.2.3-py2.py3-none-any.whl
@@ -34,10 +38,22 @@ For example to download Execution Server 2022.2 use command:
 `wget https://www.python.org/ftp/python/3.9.9/Python-3.9.9.tgz`
 
 
-### Adding Xamarin repo
+### Adding Xamarin repo (CentOS/RHEL)
 `curl https://download.mono-project.com/repo/centos7-stable.repo | tee /etc/yum.repos.d/mono-centos7-stable.repo`
+
+### Adding Xamarin repo (Ubuntu)
+apt install gnupg ca-certificates
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb https://download.mono-project.com/repo/ubuntu stable-focal main" | sudo tee /etc/apt/sources.list.d/mono-official-stable.list
+
 ### Download RPM-packages
 `yumdownloader --resolve yum-utils make mono-complete-5.16.0.220 gcc python2-devel python3-devel openssl-devel zlib zlib-devel libffi-devel python2-pip python3-pip`
+
+### Download DEB-packages
+`apt install --download-only build-essential zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev libssl-dev libreadline-dev libffi-dev libsqlite3-dev wget libbz2-dev zlib* python-pip mono-complete=6.12.* -y`
+
+Find packages at `/var/cache/apt/archives`
+
 ### Download Python-packages
 #### Python 2 packages
 ```
@@ -47,7 +63,7 @@ pip2 download wheel==0.37.1
 ```
 #### Python 3 packages
 ```
-pip2 download pip==21.2.4
-pip2 download virtualenv==20.13.0
-pip2 download wheel==0.37.1
+pip3 download pip==21.2.4
+pip3 download virtualenv==20.13.0
+pip3 download wheel==0.37.1
 ```
--- cloudshell_es_install_offline_script.sh
+++ cloudshell_es_install_offline_script.sh
@@ -7,13 +7,14 @@ Help()
     # Display Help
     echo "Execution Server installation."
     echo
-    echo "Syntax: $0 [-h|s|u|p|n]"
+    echo "Syntax: $0 [-h|s|u|p|n|o]"
     echo "Mandatory options:"
     echo "h     Print this Help."
     echo "s     Cloudshell Server Address."
     echo "u     Cloudshell Server User."
     echo "p     Cloudshell Server Password."
     echo "n     Execution Server Name."
+    echo "o     Operation System. Possible values: rhel, ubuntu"
     echo
 }
 
@@ -37,6 +38,28 @@ configure_systemctl_service() {
 	systemctl enable es
 }
 
+install_rpm_packages() {
+    # Try to remove existed mono installation
+    echo "Uninstalling old Mono..."
+    yum remove mono
+    yum autoremove
+
+    # Install all necessary rpm-packages
+    echo "Installing all necessary RPM-packages... "
+    yum --disablerepo=* localinstall $SCRIPT_PATH/os_pkgs/*.rpm -y --skip-broken
+}
+
+install_deb_packages() {
+    # Try to remove existed mono installation
+    echo "Uninstalling old Mono..."
+    apt remove mono
+    apt autoremove -y
+
+    # Install all necessary deb-packages
+    echo "Installing all necessary DEB-packages... "
+    apt install $SCRIPT_PATH/os_pkgs/* -y
+}
+
 install_python2718() {
     echo "Installing Python 2.7.18"
     cd /usr/src
@@ -47,11 +70,11 @@ install_python2718() {
     make altinstall
     rm -f /usr/src/Python-2.7.18.tgz
 
-    /usr/local/bin/python2.7 -m ensurepip  # will install pip and setuptools
-    # Install required packages for the QsDriverHost
-    /usr/local/bin/python2.7 -m pip install --no-index --find-links $ES_INSTALL_PATH/packages/VirtualEnvironment/ -r $ES_INSTALL_PATH/packages/VirtualEnvironment/requirements.txt
     # create symlink for python
     ln -s /usr/local/bin/python2.7 /usr/local/bin/python
+
+    # Install pip and setuptools
+    /usr/local/bin/python -m ensurepip
 }
 
 
@@ -86,11 +109,23 @@ install_python3() {
 
 if [ ! -f $SCRIPT_PATH/ExecutionServer.tar ]
 then
-    echo -e "\033[0;31m File does not exist in Bash \033[0m";
+    echo -e "\033[0;31m File $SCRIPT_PATH/ExecutionServer.tar does not exist \033[0m";
     exit 1
 fi
 
-while getopts h:s:u:p:n: flag
+if [ ! -d $SCRIPT_PATH/os_pkgs ]
+then
+    echo -e "\033[0;31m Directory $SCRIPT_PATH/os_pkgs DOES NOT exists. \033[0m"
+    exit 2
+fi
+
+if [ ! -d $SCRIPT_PATH/python_pkgs ]
+then
+    echo -e "\033[0;31m Directory $SCRIPT_PATH/python_pkgs DOES NOT exists. \033[0m"
+    exit 2
+fi
+
+while getopts h:s:u:p:n:o: flag
 do
     case "${flag}" in
         h) # Display Help and exit
@@ -99,6 +134,7 @@ do
         u) cs_server_user=${OPTARG};; # Set Cloudshell Server Username
         p) cs_server_pass=${OPTARG};; # Set Cloudshell Server Password
         n) es_name=${OPTARG};;        # Set Execution Server Name
+        o) os=${OPTARG};;             # Set Operation System
         \?) # Raise error, display Help and exit
            echo -e "\033[0;31m Error: Invalid option provided \033[0m"; Help; exit 1;;
     esac
@@ -126,33 +162,57 @@ if [ -z "$cs_server_pass" ]; then
     cs_server_pass="admin"
 fi
 
+if [ -z "$os" ]; then
+    echo -e "\033[0;33m Operation System is not specified. \033[0m";
+    exit 1
+else
+    if [[ "$os" != "rhel" ]] && [[ "$os" != "ubuntu" ]]; then
+        echo -e "\033[0;33m Unsupported Operation System. Possible values: rhel, ubuntu. \033[0m";
+    fi
+fi
+
 # Create installation directory
 mkdir -p $ES_INSTALL_PATH
 
 # Unpack ExecutionServer archive to installation directory
 tar -xf ExecutionServer.tar -C $ES_INSTALL_PATH
 
-# Try to remove existed mono installation
-echo "Uninstalling old Mono..."
-yum remove mono
-yum autoremove
 
-# Install all necessary rpm-packages
-echo -n "Installing all necessary RPM-packages... "
-yum --disablerepo=* localinstall ./rpm_pkgs/*.rpm -y --skip-broken
+if [[ "$os" == "rhel" ]]; then
+    install_rpm_packages
+else
+    install_deb_packages
+
+    lsb_release_path=$(which lsb_release)
+    if [ -n "$lsb_release_path" ]; then
+        mv $lsb_release_path $lsb_release_path.backup
+    fi
+
+fi
+
 
 # Install Python 2
-echo -n "checking if Python 2.7.18 is installed... "
-version=$(python -V 2>&1 | grep -Po '(?<=Python )(.+)')
-parsedVersion=$(echo "${version//./}")
-if [[ "$parsedVersion" -ne "2718" ]]
-then
-    echo "no"
-    install_python2718
+echo -n "Checking if Python 2.7.18 is installed... "
+version=$(python -V 2>&1 | grep -Po "(?<=Python )(.+)" | sed "s/\.//g")
+if [ -z "$version" ]; then
+    version=$(python2 -V 2>&1 | grep -Po "(?<=Python )(.+)" | sed "s/\.//g")
+    if [[ "$version" -ne "2718" ]]
+    then
+        echo "no"
+        install_python2718
+    else
+        ln -s $(which python2) /usr/local/bin/python
+    fi
+else
+    if [[ "$version" -ne "2718" ]]
+    then
+        echo "no"
+        install_python2718
+    fi
 fi
 
 # Install Python 3
-echo -n "checking if Python 3 is installed... "
+echo -n "Checking if Python 3 is installed... "
 if ! [type python3 &> /dev/null]
     then
         echo "no"
@@ -160,6 +220,9 @@ if ! [type python3 &> /dev/null]
 else
     echo "yes"
 fi
+
+# Install required packages for the QsDriverHost
+/usr/local/bin/python -m pip install --no-index --find-links $ES_INSTALL_PATH/packages/VirtualEnvironment/ -r $ES_INSTALL_PATH/packages/VirtualEnvironment/requirements.txt
 
 # Install python packages
 /usr/local/bin/python -m pip install --no-index --find-links $SCRIPT_PATH/python_pkgs $SCRIPT_PATH/python_pkgs/pip-19.2.3-py2.py3-none-any.whl -U
@@ -169,7 +232,7 @@ python3 -m pip install --no-index --find-links $SCRIPT_PATH/python_pkgs $SCRIPT_
 python3 -m pip install --no-index --find-links $SCRIPT_PATH/python_pkgs $SCRIPT_PATH/python_pkgs/virtualenv-20.13.0-py2.py3-none-any.whl -U
 python3 -m pip install --no-index --find-links $SCRIPT_PATH/python_pkgs $SCRIPT_PATH/python_pkgs/wheel-0.37.1-py2.py3-none-any.whl -U
 
-# configure the execution server as a service
+# Configure the execution server as a service
 configure_systemctl_service
 
 echo "Starting execution server service"
